aa8a47526b8836bd0e8cd8279cf9ab6de429243b
Sam Reigle <sreigle@ackmanndickenson.com>
2016-06-22 09:16:06 -0500

    add viewport meta tag

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <title><?php wp_title(); ?></title>
   <?php wp_head(); ?>
