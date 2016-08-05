0c62daaabd6067ca3b1e5af24ab6ed8dcd5fbb15
Sam Reigle <sreigle@ackmanndickenson.com>
2016-08-05 10:08:15 -0500

    style searchform

---
+++ searchform.php
@@ -0,0 +1,12 @@
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo get_home_url(); ?>">
+  <div class="form-group">
+    <div class="input-group">
+      <input class="form-control" type="text" value="" name="s" id="s" placeholder="Search">
+      <div class="input-group-btn">
+        <button class="btn btn-default" type="submit">
+          <span class="glyphicon glyphicon-search"></span>
+        </button>
+      </div>
+    </div>
+  </div>
+</form>
