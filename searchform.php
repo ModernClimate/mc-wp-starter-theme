4348cb40813919328d684fae36d10e3817cb91a3
Justin Rhodes <justin.m.rhodes@gmail.com>
2016-10-04 13:07:21 -0500

    added bootstrap search form and adjusted variable fonts path

---
+++ searchform.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Default search form.
+ *
+ * @package AD Starter
+ */
+?>
+
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo get_home_url(); ?>">
+	<div class="form-group">
+		<div class="input-group">
+			<input type="text" class="form-control"
+			       placeholder="<?php _e( 'Search …', 'ad-starter' ) ?>"
+			       value="<?php echo get_search_query() ?>" name="s"
+			       title="<?php _e( 'Search for:', 'ad-starter' ) ?>"/>
+			<div class="input-group-btn">
+				<button class="btn btn-default" type="submit">
+					<span class="glyphicon glyphicon-search"></span>
+				</button>
+			</div>
+		</div>
+	</div>
+</form>
