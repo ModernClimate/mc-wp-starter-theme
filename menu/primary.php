64a70b8604078a211c73710fbc694e3e90c03540
Justin Rhodes <justin.m.rhodes@gmail.com>
2016-10-10 14:36:20 -0500

    Primary Walker Menu for bootstrap navigation

--- menu/primary.php
+++ menu/primary.php
@@ -4,6 +4,9 @@
  *
  * @package AD Starter
  */
+
+use AD\App\Menus\PrimaryMenuWalker;
+
 ?>
 
 <?php if ( has_nav_menu( 'primary' ) ) : // Check if there's a menu assigned to the 'primary' location. ?>
@@ -15,7 +18,8 @@
 			'container_class' => 'collapse navbar-collapse',
 			'container_id'    => 'primary-menu',
 			'fallback_cb'     => false,
-			'depth'           => 1
+			'depth'           => 2,
+			'walker'          => new PrimaryMenuWalker()
 		] );
 		?>
 	</div><!-- /#menu-primary -->
