ac3546581de3168d64a9098363a55c7e72f18db0
Justin Rhodes <justin.m.rhodes@gmail.com>
2017-02-01 11:17:31 -0600

    Added transients helper class

---
+++ App/Core/Transients.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AD\App\Core;
+
+use AD\App\Interfaces\WordPressHooks;
+
+/**
+ * Class Transients
+ *
+ * Class for handling transient data in the theme
+ *
+ * @package AD\App\Core
+ */
+class Transients implements WordPressHooks {
+
+    const KEY = 'ad_transient_keys';
+
+    public $transientKeys = [];
+
+    /**
+     * Setup the Post Transients
+     */
+    public function addHooks() {
+        add_action( 'save_post', [ $this, 'deleteTransients' ] );
+        add_action( 'create_term', [ $this, 'deleteTransients' ], 10, 3 );
+        add_action( 'edit_term', [ $this, 'deleteTransients' ], 10, 3 );
+        add_action( 'delete_term', [ $this, 'deleteTransients' ], 10, 3 );
+    }
+
+    /**
+     * Store all transient keys in an option array
+     *
+     * @param string $key
+     */
+    public function registerTransients( $key ) {
+        $this->transientKeys = get_option( self::KEY, [] );
+
+        if ( ! in_array( $key, $this->transientKeys ) ) {
+            $this->transientKeys[] = $key;
+        }
+
+        update_option( self::KEY, $this->transientKeys );
+    }
+
+    /**
+     * Delete the stored transients
+     *
+     * @param string
+     */
+    public function deleteTransients() {
+        $this->transientKeys = get_option( self::KEY, [] );
+
+        foreach ( $this->transientKeys as $key ) {
+            delete_transient( $key );
+        }
+    }
+}
