f00e5c09abff62b94d23755f565bb1e2a0c9380e
Alec Rippberger <arippberger@gmail.com>
2018-01-23 12:08:39 -0600

    add ACF field helpers

--- App/ACF.php
+++ App/ACF.php
@@ -11,6 +11,16 @@ use AD\App\Interfaces\WordPressHooks;
  */
 class ACF implements WordPressHooks {
 
+    /**
+     * Holds any ACF fields that should allow a helper image.
+     * Modify this list to allow helper images on additional fields.
+     *
+     * @var array
+     */
+    private $helper_image_fields = [
+        'message'
+    ];
+
     public function __construct() {
         // load ACF Fields
         require_once AD_THEME_DIR . 'inc/acf/fields.php';
@@ -21,6 +31,10 @@ class ACF implements WordPressHooks {
      */
     public function addHooks() {
         add_action( 'init', [ $this, 'addOptionsPage' ] );
+
+        // Add helper images.
+        add_action( 'acf/render_field_settings', [ $this, 'addImage' ] );
+        add_action( 'acf/render_field', [ $this, 'renderImage' ] );
     }
 
 
@@ -98,4 +112,195 @@ class ACF implements WordPressHooks {
 
         return $acf_options;
     }
+
+    /**
+     * Helper function for retrieving a single option.
+     *
+     * @param $option
+     *
+     * @return mixed|null
+     */
+    public static function getACFOption( $option ) {
+
+        $acf_options = self::getACFOptions();
+
+        return isset( $acf_options[ $option ] ) ? $acf_options[ $option ] : null;
+    }
+
+    /**
+     * Returns an option wrapped in HTML. Not conducive to anchors, images, etc. Does work well for
+     * headings, spans, divs, etc.
+     *
+     * @param       $option
+     * @param       $element
+     * @param array $atts
+     * @param bool  $escape
+     *
+     * @return string
+     */
+    public static function getOptionHTML( $option, $element, $atts = [], $escape = true ) {
+        $option = self::getACFOption( $option );
+
+        return self::getHTML( $option, $element, $atts, $escape );
+    }
+
+    /**
+     * Returns an ACF subfield wrapped in HTML. Automatically escapes content.
+     * Example usage: echo ACF::getSubFieldHTML('heading', 'h1', ['class'=>'block__heading', 'id'=>'my-heading']);
+     * Example usage: echo ACF::getSubFieldHTML('wysiwyg_content', 'div', ['class'=>'block__content wysiwyg'], false);
+     *
+     * @param       $subfield
+     * @param       $element
+     * @param array $atts
+     * @param bool  $escape
+     *
+     * @return string - an HTML element.
+     */
+    public static function getSubFieldHTML( $subfield, $element, $atts = [], $escape = true ) {
+        if ( ! function_exists( 'get_sub_field' ) ) {
+            return '';
+        }
+
+        $data = get_sub_field( $subfield );
+
+        return self::getHTML( $data, $element, $atts, $escape );
+    }
+
+    /**
+     * Wraps data in HTML w/ optional attributes / escaping.
+     *
+     * @param       $data         - the content (typically text) to wrap
+     * @param       $element      - the HTML element to wrap the content with
+     * @param array $atts         - any attributes that should be added to the HTML element
+     * @param bool  $escape       - whether to escape $data - defaults to true
+     * @param bool  $self_closing - whether the element is self closing i.e. <img />
+     *
+     * @return string - an HTML element.
+     */
+    public static function getHTML( $data = null, $element = 'span', $atts = [], $escape = true, $self_closing = false ) {
+        $atts_output = ' ';
+
+        // data cannot be empty without the element being self-closing
+        if ( empty( $data ) && $self_closing === false ) {
+            return '';
+        }
+
+        if ( $escape ) {
+            $data = esc_html( $data );
+        }
+
+        foreach ( $atts as $key => $att ) {
+
+            // do not proceed if key is empty
+            if ( empty( $key ) ) {
+                continue;
+            }
+
+            // if key is present and attribute is empty, add only key to output.
+            // This allows for HTML5 boolean attributes.
+            // e.g. <input type="checkbox" checked disabled>Test</input>
+            if ( ! isset( $att ) || empty( $att ) ) {
+                $atts_output .= esc_attr( $key ) . ' ';
+                continue;
+            }
+
+            $atts_output .= esc_attr( $key ) . '="' . esc_attr( $att ) . '" ';
+        }
+
+        return $self_closing ? '<' . $element . $atts_output . ' />' : '<' . $element . $atts_output . '>' . $data . '</' . $element . '>';
+    }
+
+    /**
+     * Helper/wrapper function that makes dealing with ACF image arrays easier.
+     * Grabs the required data from the ACF image array and passes it to self::getHTML().
+     * Assumes ACF image field is configured to save data as an image array.
+     * Default data can be overwritten if passed in $atts.
+     * Example usage: echo ACF::getImageHTML( $slide['image'], [ 'class' => 'slide__img' ], 'large' );
+     *
+     * @param        $acf_image_array
+     * @param array  $atts
+     * @param string $size
+     *
+     * @return string - an HTML img element.
+     */
+    public static function getImageHTML( $acf_image_array, $atts = [], $size = 'medium' ) {
+
+        if ( ! isset( $acf_image_array['sizes'][ $size ] ) ) {
+            return '';
+        }
+
+        if ( ! isset( $atts['src'] ) ) {
+            $atts['src'] = $acf_image_array['sizes'][ $size ];
+        }
+
+        if ( ! isset( $atts['alt'] ) ) {
+            $atts['alt'] = $acf_image_array['alt'];
+        }
+
+        if ( ! isset( $atts['height'] ) ) {
+            $atts['height'] = $acf_image_array['sizes'][ $size . '-height' ];
+        }
+
+        if ( ! isset( $atts['width'] ) ) {
+            $atts['width'] = $acf_image_array['sizes'][ $size . '-width' ];
+        }
+
+        return self::getHTML( null, 'img', $atts, false, true );
+    }
+
+    /**
+     * Add the extra helper image field (when configuring ACF fields).
+     * A helper image is useful in portraying (i.e. through a screenshot)
+     * how a field will be represented on the frontend.
+     *
+     * @param $field
+     */
+    public function addImage( $field ) {
+
+        if ( isset( $field['type'] ) && ! in_array( $field['type'], $this->helper_image_fields ) ) {
+            return;
+        }
+
+        if ( ! function_exists( 'acf_render_field' ) ) {
+            return;
+        }
+
+        acf_render_field_setting( $field, [
+            'label'        => __( 'Helper Image', 'summit' ),
+            'instructions' => __( 'Upload an image that will help a user discern what this field is used for.', 'summit' ),
+            'name'         => 'helper_image',
+            'type'         => 'image',
+            'ui'           => 1,
+        ], true );
+    }
+
+    /**
+     * Renders the helper image within the ACF field (when editing a post).
+     *
+     * @param $field
+     *
+     * @return mixed
+     */
+    public function renderImage( $field ) {
+
+        if ( isset( $field['type'] ) && ! in_array( $field['type'], $this->helper_image_fields ) ) {
+            return $field;
+        }
+
+        // bail early if no 'admin_only' setting
+        if ( ! isset( $field['helper_image'] ) || empty( $field['helper_image'] ) ) {
+            return $field;
+        }
+
+        $image = wp_get_attachment_image_src( $field['helper_image'], 'medium' );
+
+        if ( ! isset( $image[0] ) ) {
+            return $field;
+        }
+
+        echo '<img src="' . esc_url( $image[0] ) . '" alt="' . __( 'Helper Image', 'summit' ) . '" height="' . esc_attr( $image[2] ) . '" width="' . esc_attr( $image[1] ) . '" />';
+
+        return $field;
+    }
+
 }
