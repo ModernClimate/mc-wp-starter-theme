52d8446419d32a671093af94aaad268404e8267c
Rob Abbott <rob@headsupinteractive.com>
2020-08-05 16:49:04 -0500

    add hero edit component

--- blocks/src/hero/index.js
+++ blocks/src/hero/index.js
@@ -5,12 +5,17 @@ import { __ } from "@wordpress/i18n";
 import { MediaPlaceholder } from "@wordpress/block-editor";
 import { Fragment } from "@wordpress/element";
 
+/**
+ * Internal dependencies
+ */
+import edit from "./edit";
+
 const name = "ad-blocks/hero";
 
 const settings = {
-  title: __("Hero"),
+  title: __("Hero", "ad-starter"),
   category: "common",
-  description: __("Large banner image"),
+  description: __("Large banner image", "ad-starter"),
   icon: "format-image",
   supports: {
     align: true,
@@ -21,37 +26,17 @@ const settings = {
       type: "string",
       source: "attribute",
       attribute: "data-bkg",
-      selector: ".hero__image",
+      selector: ".ad-hero__image",
     },
   },
   example: {},
-  edit: ({ attributes, setAttributes, className }) => {
-    const { MediaAsset } = attributes;
-
-    return (
-      <Fragment>
-        <div className="ad-block__hero">
-          {MediaAsset ? (
-            <img src={MediaAsset} />
-          ) : (
-            <MediaPlaceholder
-              onSelect={(media) => {
-                setAttributes({ MediaAsset: media.url });
-              }}
-              allowedTypes={["image"]}
-              labels={{ title: "Hero Image" }}
-            />
-          )}
-        </div>
-      </Fragment>
-    );
-  },
+  edit,
   save: ({ attributes }) => {
     return (
       <Fragment>
-        <div className="ad-block__hero hero">
+        <div className="ad-hero">
           <div
-            className="hero__image bg-defer"
+            className="ad-hero__image bg-defer"
             data-bkg={attributes.MediaAsset}
           ></div>
         </div>
