256995e78dea29b3a7f815b93d2457112f4b2848
Jesse Andersen <gojsse@gmail.com>
2022-03-31 17:44:03 -0500

    Update gulp scripts (#62)

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,29 @@
 const gulp = require('gulp');
-const phpcs = require('gulp-phpcs');
 
-// Theme tasks
-const styles = require('./gulp-tasks/styles');
-const themeScripts = require('./gulp-tasks/themeScripts');
+// Import tasks
+const taskDir = './gulp-tasks';
+const styles = require(`${taskDir}/styles`);
+const themeScripts = require(`${taskDir}/themeScripts`);
+const php = require(`${taskDir}/php`);
 
-const PATHS = {
-  php: [
-    '**/*.php',
-    '!blocks/**/*.*',
-    '!vendor/**/*.*'
-  ]
-};
-
-// Run php code sniffer on theme files
-function phpCodeSniffer() {
-  return (
-    gulp.src(PATHS.php)
-      .pipe(phpcs({
-        bin: './vendor/squizlabs/php_codesniffer/bin/phpcs',
-        standard: 'PSR2',
-        warningSeverity: 0,
-        colors: 1,
-        ignore: ['node_modules', 'build']
-      }))
-      .pipe(phpcs.reporter('log'))
-  );
-}
-
+// Define grouped tasks
 const watch = gulp.parallel(styles.watch, themeScripts.watch);
-const build = gulp.series(styles.process, themeScripts.process, phpCodeSniffer);
+const watchDev = gulp.parallel(styles.watchDev, themeScripts.watchDev);
+const build = gulp.series(styles.prod, themeScripts.prod, php.codeSniffer);
+const buildDev = gulp.series(styles.dev, themeScripts.dev, php.codeSniffer);
+const all = gulp.series(styles.prod, styles.dev, themeScripts.prod, themeScripts.dev, php.codeSniffer);
 
+exports.default = all;
 exports.watch = watch;
+exports.watchDev = watchDev;
 exports.build = build;
-exports.default = build;
+exports.buildDev = buildDev;
+
+exports.js = themeScripts.prod;
+exports.jsDev = themeScripts.dev;
+
+exports.styles = styles.prod;
+exports.stylesDev = styles.dev;
+
+exports.phpcs = php.codeSniffer;
+exports.phpcbf = php.codeBeautifier;
