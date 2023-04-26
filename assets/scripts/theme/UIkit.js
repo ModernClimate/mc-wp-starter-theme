70b4f8d3d4b51187e66ee0bf31913253cf82fe59
Jesse <gojsse@gmail.com>
2023-04-26 15:25:38 -0500

    Improve comments regarding adding parallax modules to project

--- assets/scripts/theme/UIkit.js
+++ assets/scripts/theme/UIkit.js
@@ -1,16 +1,18 @@
 import UIkit from 'uikit/dist/js/uikit-core';
-// import Parallax from 'uikit/src/js/components/parallax';
 import Slider from 'uikit/src/js/components/slider';
-// import SliderParallax from 'uikit/src/js/components/slider-parallax';
 import Slideshow from 'uikit/src/js/components/slideshow';
+// To enable parallax functionality, uncomment the desired line(s) below
+// import Parallax from 'uikit/src/js/components/parallax';
+// import SliderParallax from 'uikit/src/js/components/slider-parallax';
 // import SlideshowParallax from 'uikit/src/js/components/slideshow-parallax';
 
 // Add individual components imported from UIKit below
 const components = [
-  // { name: 'parallax', component: Parallax },
   { name: 'slider', component: Slider },
-  // { name: 'slider-parallax', component: SliderParallax },
   { name: 'slideshow', component: Slideshow },
+  // To enable parallax functionality, uncomment the desired line(s) below
+  // { name: 'parallax', component: Parallax },
+  // { name: 'slider-parallax', component: SliderParallax },
   // { name: 'slideshow-parallax', component: SlideshowParallax },
 ];
 
