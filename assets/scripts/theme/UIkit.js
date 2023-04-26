dcda6e6b7c926d6dbad254aba496afcd94bb6c51
Jesse Andersen <gojsse@gmail.com>
2023-04-26 14:43:05 -0500

    Missing primary id on page builder (#82)
    * Update uikit included layout, js uikit parallax example imports
    
    * Missing id="primary" on page-builder.php

--- assets/scripts/theme/UIkit.js
+++ assets/scripts/theme/UIkit.js
@@ -1,11 +1,17 @@
 import UIkit from 'uikit/dist/js/uikit-core';
+// import Parallax from 'uikit/src/js/components/parallax';
 import Slider from 'uikit/src/js/components/slider';
+// import SliderParallax from 'uikit/src/js/components/slider-parallax';
 import Slideshow from 'uikit/src/js/components/slideshow';
+// import SlideshowParallax from 'uikit/src/js/components/slideshow-parallax';
 
 // Add individual components imported from UIKit below
 const components = [
+  // { name: 'parallax', component: Parallax },
   { name: 'slider', component: Slider },
+  // { name: 'slider-parallax', component: SliderParallax },
   { name: 'slideshow', component: Slideshow },
+  // { name: 'slideshow-parallax', component: SlideshowParallax },
 ];
 
 components.forEach(({ name, component }) => {
