562d471ec47ce2d56683946f320c8333c526e7cc
Charlie Detviler <cdetviler-ext@beachbody.com>
2023-09-14 13:45:59 -0500

    add-module-generation: adds shell script to generate module files

---
+++ module_generation/template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * ACF Module: PASCAL_CASE_MODULE_NAME
+ *
+ * @global $data
+ */
+
+use NAME_SPACE_ID\App\Fields\ACF;
+use NAME_SPACE_ID\App\Fields\Util;
+
+do_action('NAME_SPACE_ID_LOWERCASE/modules/styles', $row_id, $data);
+?>
+
+<div class="module KEBAB_CASE_MODULE_NAME" id="<?php echo esc_attr($row_id); ?>">
+<!-- templated stuff here -->
+</div>
+
